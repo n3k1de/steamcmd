3cb9f8a9eea1c74d585298df8466a948ec00f563
DJustDE <mail@djust.de>
2019-12-17 00:16:34 +0100

    Update entrypoint.sh

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -3,6 +3,7 @@ ulimit -n 2048
 cd /home/steam/steamcmd
 if [ "$(stat -c %U /home/steam/steamcmd)" != "steam" ] ; then 
   chown -R steam.steam /home/steam/steamcmd /home/steam/server
+  chmod -v 755 /home/steam/steamcmd /home/steam/server
 fi;
 # wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
 wget -O start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/start-gmod.sh
