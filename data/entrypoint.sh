df02e0ffe962a08b1cae7f83d066ed4c9e5fe2c1
DJustDE <mail@djust.de>
2019-12-16 23:55:01 +0100

    Update and rename entrypoint to entrypoint.sh

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -3,6 +3,7 @@ cd ${STEAMCMDDIR}
 if [ "$(stat -c %U ${STEAMCMDDIR})" != "steam" ] ; then 
   chown -R steam.steam ${STEAMCMDDIR}
 fi;
-wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
+# wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
+wget -O start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/start-gmod.sh
 chmod -R 0775 ./start.sh
 su steam -c "./start.sh"
