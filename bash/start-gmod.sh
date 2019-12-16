736a1cef0df7d65f031d7b44872da2a0e0ed2380
DJustDE <mail@djust.de>
2019-12-17 00:12:56 +0100

    Update start-gmod.sh

--- bash/start-gmod.sh
+++ bash/start-gmod.sh
@@ -3,8 +3,8 @@
 # update
 ${STEAMCMDDIR}/steamcmd.sh +login anonymous \
  +force_install_dir "${SERVERDIR}/gmod/" +app_update 4020 validate \
- +force_install_dir "${SERVERDIR}/css" +app_update 232330 +validate \
- +force_install_dir "${SERVERDIR}/tf2" +app_update 232250 +validate \
+ +force_install_dir "${SERVERDIR}/css" +app_update 232330 validate \
+ +force_install_dir "${SERVERDIR}/tf2" +app_update 232250 validate \
  +quit
 # ln -s ${SERVERDIR}/linux32/steamclient.so ${SERVERDIR}/.steam/sdk32/steamclient.so
 
