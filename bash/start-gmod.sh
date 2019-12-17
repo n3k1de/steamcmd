20fd1feb10a27062d2b7c0894e37c448fe2f8e87
DJustDE <mail@djust.de>
2019-12-17 18:33:35 +0100

    Update start-gmod.sh

--- bash/start-gmod.sh
+++ bash/start-gmod.sh
@@ -3,10 +3,12 @@
 # update
 ${STEAMCMDDIR}/steamcmd.sh +login anonymous \
  +force_install_dir "${SERVERDIR}/gmod/" +app_update 4020 validate \
- +force_install_dir "${SERVERDIR}/css" +app_update 232330 validate \
- +force_install_dir "${SERVERDIR}/tf2" +app_update 232250 validate \
  +quit
+
+# +force_install_dir "${SERVERDIR}/css" +app_update 232330 validate \
+# +force_install_dir "${SERVERDIR}/tf2" +app_update 232250 validate \
 
 # server start
 cd ${SERVERDIR}/gmod/
+ls
 $./srcds_run -game garrysmod -secure +sv_setsteamaccount ${SERVERACCOUNT} -authkey ${APIKEY} +host_workshop_collection ${WORKSHOPCOLLECTION} -port ${PORT} +tv_port ${PORTTV} -clientport ${CLIENTPORT} -sport ${SPORT} +maxplayers ${MAXPLAYERS} +gamemode ${GAMEMODE} +map ${MAP} -tickrate 66 -exec server.cfg
