a84f58bf05100c2e94cb347c7c55597699bb184e
DJustDE <mail@djust.de>
2019-12-18 15:52:18 +0100

    Update entrypoint.sh

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ulimit -n 2048
-cd /home/steam/steamcmd
+cd ${STEAMCMDDIR}
 
 # wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
 wget -O start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/start-gmod.sh
