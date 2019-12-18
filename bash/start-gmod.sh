bbdb030c2d7105016a3783ae391b38ec2238384c
DJustDE <mail@djust.de>
2019-12-18 15:53:18 +0100

    Update start-gmod.sh

--- bash/start-gmod.sh
+++ bash/start-gmod.sh
@@ -11,6 +11,4 @@ ${STEAMCMDDIR}/steamcmd.sh +login anonymous \
 # +force_install_dir "${SERVERDIR}/tf2" +app_update 232250 -validate \
 
 # server start
-cd ${SERVERDIR}/gmod/
-ls
-./srcds_run -condebug -game garrysmod -secure +sv_lan 0 +sv_setsteamaccount ${SERVERACCOUNT} -authkey ${APIKEY} +host_workshop_collection ${WORKSHOPCOLLECTION} +port ${PORT} +tv_port ${PORTTV} +clientport ${CLIENTPORT} +maxplayers ${MAXPLAYERS} +gamemode ${GAMEMODE} +map ${MAP} -tickrate 66 -exec server.cfg
+cd ${SERVERDIR}/gmod/ && ./srcds_run -condebug -game garrysmod -secure +sv_lan 0 +sv_setsteamaccount ${SERVERACCOUNT} -authkey ${APIKEY} +host_workshop_collection ${WORKSHOPCOLLECTION} +port ${PORT} +tv_port ${PORTTV} +clientport ${CLIENTPORT} +maxplayers ${MAXPLAYERS} +gamemode ${GAMEMODE} +map ${MAP} -tickrate 66 -exec server.cfg
