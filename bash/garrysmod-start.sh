c5537f10bf142d51e65187bb46f0843c5a27ed48
DJustDE <mail@djust.de>
2019-12-31 23:30:30 +0100

    Update garrysmod-start.sh

--- bash/garrysmod-start.sh
+++ bash/garrysmod-start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 # update
-rm /home/steam/.steam/sdk32/steamclient.so
-ln -s ${STEAMCMDDIR}/linux32/steamclient.so /home/steam/.steam/sdk32/steamclient.so
+if [ -e "/home/steam/.steam/sdk32/steamclient.so" ]
+then
+	echo "steamclient.so found."
+else
+	echo "steamclient.so not found."
+  ln -s ${STEAMCMDDIR}/linux32/steamclient.so /home/steam/.steam/sdk32/steamclient.so
+fi
+
 ${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType linux +login anonymous \
 +force_install_dir "${SERVERDIR}/gmod/" +app_update 4020 validate \
 +force_install_dir "${SERVERDIR}/css" +app_update 232330 validate \
