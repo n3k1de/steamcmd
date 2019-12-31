4880574eb8872ad2bb665f8261ae2c525015acef
DJustDE <mail@djust.de>
2019-12-31 18:50:01 +0100

    Delete entrypoint.sh

--- data/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ulimit -n 2048
-cd ${STEAMCMDDIR}
-
-# wget -O start.sh https://play.djust.de/steamcmd/request/ttt/${HOSTNAME}/start.sh
-wget -O start.sh https://raw.githubusercontent.com/djust-de/steamcmd/master/bash/start-gmod.sh
-chmod -R 0755 ./start.sh
-./start.sh
